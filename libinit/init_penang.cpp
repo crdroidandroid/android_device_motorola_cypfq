5e308366ca6c80ad92168c8e341e3d53c68e8aea
xyyx <xyyx@mail.ru>
2023-11-13 23:12:01 +0900

    penang: init: Add SafetyNet workaround

--- libinit/init_penang.cpp
+++ libinit/init_penang.cpp
@@ -6,6 +6,9 @@
 
 #include <libinit_dalvik_heap.h>
 #include <libinit_variant.h>
+#include <libinit_utils.h>
+
+#include <sys/stat.h>
 
 #include "vendor_init.h"
 
@@ -30,7 +33,56 @@ static const std::vector<variant_info_t> variants = {
     ym_info,
 };
 
+/* From Magisk@jni/magiskhide/hide_utils.c */
+static const char *snet_prop_key[] = {
+    "ro.boot.vbmeta.device_state",
+    "ro.boot.verifiedbootstate",
+    "ro.boot.flash.locked",
+    "ro.boot.selinux",
+    "ro.boot.veritymode",
+    "ro.boot.warranty_bit",
+    "ro.warranty_bit",
+    "ro.debuggable",
+    "ro.secure",
+    "ro.build.type",
+    "ro.build.tags",
+    "ro.build.selinux",
+    "sys.oem_unlock_allowed",
+    NULL
+};
+
+static const char *snet_prop_value[] = {
+    "locked",
+    "green",
+    "1",
+    "enforcing",
+    "enforcing",
+    "0",
+    "0",
+    "0",
+    "1",
+    "user",
+    "release-keys",
+    "1",
+    "0",
+    NULL
+};
+
+static void workaround_snet_properties() {
+
+    // Hide all sensitive props
+    for (int i = 0; snet_prop_key[i]; ++i) {
+        property_override(snet_prop_key[i], snet_prop_value[i]);
+    }
+
+    chmod("/sys/fs/selinux/enforce", 0640);
+    chmod("/sys/fs/selinux/policy", 0440);
+}
+
 void vendor_load_properties() {
     search_variant(variants);
     set_dalvik_heap();
+
+    // Workaround SafetyNet
+    workaround_snet_properties();
 }
