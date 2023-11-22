a026508f94755e95ab7e12900519753a99c802f8
lahaina <nattolecats@gmail.com>
2023-11-22 21:27:09 +0900

    penang: libinit: Hide SELinux state only * Other props are up to rom implementation

--- libinit/init_penang.cpp
+++ libinit/init_penang.cpp
@@ -35,48 +35,7 @@ static const std::vector<variant_info_t> variants = {
     ym_info,
 };
 
-/* From Magisk@jni/magiskhide/hide_utils.c */
-static const char *snet_prop_key[] = {
-    "ro.boot.vbmeta.device_state",
-    "ro.boot.verifiedbootstate",
-    "ro.boot.flash.locked",
-    "ro.boot.selinux",
-    "ro.boot.veritymode",
-    "ro.boot.warranty_bit",
-    "ro.warranty_bit",
-    "ro.debuggable",
-    "ro.secure",
-    "ro.build.type",
-    "ro.build.tags",
-    "ro.build.selinux",
-    "sys.oem_unlock_allowed",
-    NULL
-};
-
-static const char *snet_prop_value[] = {
-    "locked",
-    "green",
-    "1",
-    "enforcing",
-    "enforcing",
-    "0",
-    "0",
-    "0",
-    "1",
-    "user",
-    "release-keys",
-    "1",
-    "0",
-    NULL
-};
-
 static void workaround_snet_properties() {
-
-    // Hide all sensitive props
-    for (int i = 0; snet_prop_key[i]; ++i) {
-        property_override(snet_prop_key[i], snet_prop_value[i]);
-    }
-
     chmod("/sys/fs/selinux/enforce", 0640);
     chmod("/sys/fs/selinux/policy", 0440);
 }
